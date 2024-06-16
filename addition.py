8c2724884bae9bce46e156d3b492bcedee7ad5d1
PavanMatam <matampavan.srinivas2021@vitstudent.ac.in>
2024-06-16 11:22:21 +0000

    your_commit_message

---
+++ addition.py
@@ -0,0 +1,12 @@
+# addition.py
+
+def addition():
+    a = float(input("Enter the first number (a): "))
+    b = float(input("Enter the second number (b): "))
+    c = a + b
+    print(f"{a} + {b} = {c}")
+
+if __name__ == "__main__":
+    addition()
+
+
